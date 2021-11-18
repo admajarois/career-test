ad3de098966d209b3aee96d1435e0b0c9779a1c9
admajarois <rois.admj@gmail.com>
2021-11-18 20:33:50 +0700

    some code

---
+++ app/Controllers/Register.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Register extends BaseController
+{
+    public function index()
+    {
+        $data = [
+            'title' => 'Register'
+        ];
+        return view('auth/register', $data);
+    }
+}
